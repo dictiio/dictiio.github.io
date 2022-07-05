938a11d6858f393b6a716d6819f6ed3288b853ab
dictiio <truong.steven7719@gmail.com>
2022-07-04 23:49:25 -0400

    Implemented new features

---
+++ portfolio.js
@@ -0,0 +1,31 @@
+let health = 100;
+
+function hit() {
+    health = health/2
+    if(health <= 1) {
+        alert("Game Over!");
+    }
+    else {
+        alert(health);
+    }
+};
+
+window.onload = function() {
+    var canvas = document.getElementById('canvas-one');
+    var context = canvas.getContext("2d");
+
+    context.beginPath();
+    context.rect(0, 0, 200, 150);
+    context.lineWidth = 10;
+    context.strokeStyle = "green";
+    context.fillStyle = "lime";
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.fillStyle = "white";
+    context.font = "24px Tahoma";
+    context.fillText("HELLO WORLD", 75, 125);
+
+};
+
